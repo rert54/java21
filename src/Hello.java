2128976b30454e767a2505a31a9fcaf30379b279
ril4 <rert54@gmail.com>
2026-03-27 14:43:45 +0900

    HelloTest Project

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Worild");
+		System.out.println("안녕하세요");
+		System.out.print(1+2);
+		System.out.println(1+2);
+		System.out.printf("이름:%s, 성별:%c, 나이:%d, 키:%.2f", "홍길동", '남', 20, 184.3);
+	}
+
+}
