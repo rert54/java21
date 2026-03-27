4047c4461d3eb3a11a786b48b07aaedde5314f4c
ril4 <rert54@gmail.com>
2026-03-27 14:57:19 +0900

    260327 14:57

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Worild");
+		System.out.print("Hello Worild2");
 		System.out.println("안녕하세요");
 		System.out.print(1+2);
 		System.out.println(1+2);
